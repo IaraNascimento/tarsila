75a8a851523d12ede9644677884fd5f24a6c554f
Laura de Lucca <laura.lucca@180s.com.br>
2025-10-05 23:49:59 -0300

    move requisição do past-chats para a pagina home, removendo do header

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -3,30 +3,16 @@
 import { useAuth } from "@/app/contexts/AuthProvider";
 import { useLoader } from "@/app/contexts/LoaderProvider";
 import style from "./header.module.css";
-import { useCallback, useEffect, useState } from "react";
-import { ChatType, getPastChats, PastChats } from "@/app/services/services";
+import { useState } from "react";
 import MenuButton from "../menu-button/menu-button";
 import NavMenu from "../nav-menu/nav-menu";
+import { useHistory } from "@/app/contexts/HistoryProvider";
 
 export default function Header() {
   const { isAuthenticated, currentUser, logOut } = useAuth();
+  const { chatsList } = useHistory();
   const { showLoader } = useLoader();
   const [isMenuOpen, setMenuOpen] = useState<boolean>(false);
-  const [chatsList, setChatsList] = useState<ChatType[]>([]);
-
-  const getPastChatsList = useCallback(() => {
-    if (!!currentUser && currentUser.email) {
-      getPastChats(currentUser.email)
-        .then((data) => {
-          const pastChats = data as PastChats
-          setChatsList(pastChats.chats)
-        })
-    }
-  }, [currentUser])
-
-  useEffect(() => {
-    if (isMenuOpen) getPastChatsList()
-  }, [getPastChatsList, isMenuOpen])
 
   function handleMenu() {
     setMenuOpen(!isMenuOpen)
@@ -41,9 +27,11 @@ export default function Header() {
     isAuthenticated && (
       <header className={style.header}>
         <div className={style.navContainer}>
-          <MenuButton onClick={handleMenu} />
-          {
-            isMenuOpen && <NavMenu menuItems={chatsList} />
+          { chatsList.length > 0 
+            && <>
+              <MenuButton onClick={handleMenu} />
+              { isMenuOpen && <NavMenu menuItems={chatsList} /> }
+            </>
           }
         </div>
         <div>
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,25 +1,41 @@
 "use client";
 
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import ProtectedRoute from "../auth/ProtectedRoute";
 import { useLoader } from "../contexts/LoaderProvider";
 import { useRouter } from "next/navigation";
 import { useAuth } from "../contexts/AuthProvider";
 import style from "./page.module.css";
+import { getPastChats, PastChats } from "../services/services";
+import { useHistory } from "../contexts/HistoryProvider";
 
 export default function Home() {
   const { push } = useRouter();
   const { showLoader, hideLoader } = useLoader();
-    const { isAuthenticated } = useAuth();
+  const { isAuthenticated, currentUser } = useAuth();
+  const { updateChatsList } = useHistory();
+
+  const loadChatList = useCallback(() => {
+    if (!!currentUser && currentUser.email) {
+      getPastChats(currentUser.email)
+        .then((data) => {
+          const pastChats = data as PastChats
+          if (pastChats.chats.length > 0) {
+              updateChatsList(pastChats.chats);
+          }
+        })
+    }
+  }, [currentUser, updateChatsList])
         
   useEffect(() => {
     showLoader();
     if (!isAuthenticated) {
       push("/login");
     } else {
+      loadChatList();
       hideLoader();
     }
-  }, [hideLoader, isAuthenticated, push, showLoader]);
+  }, [hideLoader, isAuthenticated, loadChatList, push, showLoader]);
 
   return (
     <ProtectedRoute>
