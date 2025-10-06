69f7c1767b554b22e12acbe25d24d743438c3290
Laura de Lucca <laura.lucca@180s.com.br>
2025-10-05 23:25:52 -0300

    cria página home com botoes para decidir criar novo chat ou retomar anterior

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,17 +3,32 @@
 import { useEffect } from "react";
 import ProtectedRoute from "../auth/ProtectedRoute";
 import { useLoader } from "../contexts/LoaderProvider";
+import { useRouter } from "next/navigation";
+import { useAuth } from "../contexts/AuthProvider";
+import style from "./page.module.css";
 
 export default function Home() {
-  const { hideLoader } = useLoader();
-
+  const { push } = useRouter();
+  const { showLoader, hideLoader } = useLoader();
+    const { isAuthenticated } = useAuth();
+        
   useEffect(() => {
-    hideLoader();
-  }, []);
+    showLoader();
+    if (!isAuthenticated) {
+      push("/login");
+    } else {
+      hideLoader();
+    }
+  }, [hideLoader, isAuthenticated, push, showLoader]);
 
   return (
     <ProtectedRoute>
-      <main>Home</main>
+      <main className={style.wrapper}>
+        <div className={style.panel}>
+          <button>Criar novo</button>
+          <button>Continuar</button>
+        </div>
+      </main>
     </ProtectedRoute>
   );
 }
